3704c7a5e3aa1314b34b9f3d335b978ef369fea2
liuyang-kevin <shadowcat.liuyang@gmail.com>
2025-11-08 00:41:37 +0000

    Deploying to gh-pages from @ liuyang-kevin/godot_shader_demos@7928a5c1169686dcf0a8ff656d779e13afedef71 🚀

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1762559716|4634626';
+const CACHE_VERSION = '1762562483|4658291';
 /** @type {string} */
 const CACHE_PREFIX = 'godot_shader_dem-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
